d84c79a446a9547b1e3b393dd25715da3d36700d
richard <richard@maqe.com>
2023-02-19 07:35:28 +0700

    features

---
+++ lib/features/todo/presentation/ui/todo_screen.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+class ToDoScreen extends StatelessWidget {
+  const ToDoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo'),
+        centerTitle: true,
+      ),
+      body: Column(),
+    );
+  }
+}
