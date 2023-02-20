d72efab9b33688eebecfd38f13d73e015a78d6ce
richard <richard@maqe.com>
2023-02-20 08:22:17 +0700

    todo api service

---
+++ lib/features/todo/data/api/itodo_api_service.dart
@@ -0,0 +1,14 @@
+import 'package:http/http.dart' as http;
+
+abstract class IToDoApiService {
+
+  Future<http.Response> getToDos(int id);
+
+  Future<http.Response> getToDo(int id);
+
+  Future<http.Response> addToDo(Map<String,dynamic> body);
+
+  Future<http.Response> updateToDo(Map<String,dynamic> queries);
+
+  Future<http.Response> deleteToDo(Map<String,dynamic> queries);
+}
---
+++ lib/features/todo/data/api/todo_api_service.dart
@@ -0,0 +1,97 @@
+
+
+import 'dart:html';
+
+import 'package:http/src/response.dart';
+import 'package:todo_app/core/mixin/uri_mixin.dart';
+import 'package:todo_app/features/todo/data/api/itodo_api_service.dart';
+import 'package:http/http.dart' as http;
+
+class ToDoApiService with UriMixin implements IToDoApiService {
+  final http.Client client;
+
+  ToDoApiService(this.client);
+  
+  @override
+  Future<Response> addToDo(Map<String, dynamic> body) async {
+
+    try {
+      final url = getUri(
+        '/api/v1/todo/addToDo'
+      );
+
+      return await http.post(url, body: body);
+
+    } catch (e) {
+      rethrow;
+    }
+    
+  }
+
+  @override
+  Future<Response> deleteToDo(Map<String, dynamic> queries) async {
+    try {
+      final url = getUri(
+        '/api/v1/todo/deleteToDo',
+        null,
+        queries
+      );
+
+      return await http.delete(url);
+
+    } catch (e) {
+      rethrow;
+    }
+    
+  }
+
+  @override
+  Future<Response> getToDos(int id) async {
+
+    try {
+      final url = getUri(
+        '/api/v1/todo/getToDos/$id'
+      );
+
+      return await http.get(url);
+
+    } catch (e) {
+      rethrow;
+    }
+    
+  }
+
+  @override
+  Future<Response> updateToDo(Map<String, dynamic> queries) async {
+
+    try {
+      final url = getUri(
+        '/api/v1/todo/updateToDo',
+        null,
+        queries
+      );
+
+      return await http.put(url);
+
+    } catch (e) {
+      rethrow;
+    }
+    
+  }
+  
+  @override
+  Future<Response> getToDo(int id) async {
+
+    try {
+      final url = getUri(
+        '/api/v1/todo/getToDo/$id'
+      );
+
+      return await http.get(url);
+
+    } catch (e) {
+      rethrow;
+    }
+  }
+  
+}
