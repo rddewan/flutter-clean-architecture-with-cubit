62b3ef29bd8fc44c64f8e57fc963b21fbec23016
richard <richard@maqe.com>
2023-02-24 08:32:07 +0700

    Add ToDo form

---
+++ lib/common/style/dimens.dart
@@ -0,0 +1,6 @@
+
+const double kSmall = 8.0;
+const double kMedium = 16;
+const double kLarge = 24;
+const double kXLarge = 32;
+const double kXXLarge = 40;
--- lib/features/todo/presentation/ui/todo_add_screen.dart
+++ lib/features/todo/presentation/ui/todo_add_screen.dart
@@ -1,6 +1,8 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:todo_app/common/style/dimens.dart';
+import 'package:todo_app/common/widget/form/custom_text_form_field.dart';
 
 class ToDoAddScreen extends StatefulWidget {
   const ToDoAddScreen({Key? key}) : super(key: key);
@@ -10,7 +12,18 @@ class ToDoAddScreen extends StatefulWidget {
 }
 
 class _ToDoAddScreenState extends State<ToDoAddScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _bodyController = TextEditingController();
+  final TextEditingController _noteController = TextEditingController();
   
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _bodyController.dispose();
+    _noteController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,10 +33,92 @@ class _ToDoAddScreenState extends State<ToDoAddScreen> {
       ),
       body: SingleChildScrollView(
         child: Form(
-          child: Column(
-            children: [],
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(kMedium),
+            child: Column(
+              children: [
+                const SizedBox(height: kMedium,),
+
+                CustomTextFormField(
+                  controller: _titleController,
+                  labelText: 'Title', 
+                  hintText: 'ToDo Title',
+                  keyboardType: TextInputType.text, 
+                  textInputAction: TextInputAction.next,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'title is empty';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    return null;
+                  },
+                ),
+
+                const SizedBox(height: kMedium,),
+
+                CustomTextFormField(
+                  controller: _titleController,
+                  labelText: 'Body', 
+                  hintText: 'ToDo Body',
+                  keyboardType: TextInputType.text, 
+                  textInputAction: TextInputAction.next,
+                  maxLines: 4,
+                  minLine: 3,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'body is empty';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    return null;
+                  },
+                ),
+
+                const SizedBox(height: kMedium,),
+
+                CustomTextFormField(
+                  controller: _titleController,
+                  labelText: 'Note', 
+                  hintText: 'ToDo Note',
+                  keyboardType: TextInputType.text, 
+                  textInputAction: TextInputAction.next,
+                  maxLines: 5,
+                  minLine: 3,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Note is empty';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    return null;
+                  },
+                ),
+
+                const SizedBox(height: kMedium,),
+
+                SwitchListTile.adaptive(
+                  title: const Text('Status'),
+                  value: false, 
+                  onChanged: (value) {
+
+                  },
+                )
+              ],
+            ),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton.small(
+        heroTag: 'saveToDo',
+        onPressed: () {
+          
+        },
+        child: const Icon(Icons.save),
       ),
     );
   }
