e677e579aac469204ceba7b15519dbd82dc7fc78
richard <richard@maqe.com>
2023-02-25 07:16:29 +0700

    todo added listener

--- lib/features/todo/presentation/ui/todo_add_screen.dart
+++ lib/features/todo/presentation/ui/todo_add_screen.dart
@@ -1,10 +1,11 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:todo_app/common/mixin/dialog_mixin.dart';
 import 'package:todo_app/common/style/dimens.dart';
 import 'package:todo_app/common/widget/form/custom_text_form_field.dart';
 import 'package:todo_app/features/todo/presentation/controller/todo_add_controller.dart';
+import 'package:todo_app/features/todo/presentation/state/todo_add_state.dart';
 
 class ToDoAddScreen extends StatefulWidget {
   const ToDoAddScreen({Key? key}) : super(key: key);
@@ -13,12 +14,12 @@ class ToDoAddScreen extends StatefulWidget {
   State<ToDoAddScreen> createState() => _ToDoAddScreenState();
 }
 
-class _ToDoAddScreenState extends State<ToDoAddScreen> {
+class _ToDoAddScreenState extends State<ToDoAddScreen> with DialogMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _bodyController = TextEditingController();
   final TextEditingController _noteController = TextEditingController();
-  
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -26,6 +27,7 @@ class _ToDoAddScreenState extends State<ToDoAddScreen> {
     _noteController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,92 +36,104 @@ class _ToDoAddScreenState extends State<ToDoAddScreen> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(kMedium),
-            child: Column(
-              children: [
-                const SizedBox(height: kMedium,),
+        child: BlocListener<ToDoAddController, ToDoAddState>(
+          listener: (context, state) {
 
-                CustomTextFormField(
-                  controller: _titleController,
-                  labelText: 'Title', 
-                  hintText: 'ToDo Title',
-                  keyboardType: TextInputType.text, 
-                  textInputAction: TextInputAction.next,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'title is empty';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    context.read<ToDoAddController>().setFormData(
-                      key: 'title', value: value,
-                    );
-                    return null;
-                  },
-                ),
-
-                const SizedBox(height: kMedium,),
-
-                CustomTextFormField(
-                  controller: _bodyController,
-                  labelText: 'Body', 
-                  hintText: 'ToDo Body',
-                  keyboardType: TextInputType.text, 
-                  textInputAction: TextInputAction.next,
-                  maxLines: 4,
-                  minLine: 3,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'body is empty';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    context.read<ToDoAddController>().setFormData(
-                      key: 'body', value: value,
-                    );
-                    return null;
-                  },
-                ),
-
-                const SizedBox(height: kMedium,),
-
-                CustomTextFormField(
-                  controller: _noteController,
-                  labelText: 'Note', 
-                  hintText: 'ToDo Note',
-                  keyboardType: TextInputType.text, 
-                  textInputAction: TextInputAction.next,
-                  maxLines: 5,
-                  minLine: 3,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Note is empty';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    context.read<ToDoAddController>().setFormData(
-                      key: 'note', value: value,
-                    );
-                    return null;
-                  },
-                ),
-
-                const SizedBox(height: kMedium,),
-
-                SwitchListTile.adaptive(
-                  title: const Text('Status'),
-                  value: context.watch<ToDoAddController>().state.todoStatus, 
-                  onChanged: (value) {
-                    context.read<ToDoAddController>().setToDoStatus(value);
-                  },
-                )
-              ],
+            if(state.isAdded) {
+              _showSuccessDialog();
+            }
+          },
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(kMedium),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: kMedium,
+                  ),
+                  CustomTextFormField(
+                    controller: _titleController,
+                    labelText: 'Title',
+                    hintText: 'ToDo Title',
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'title is empty';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      context.read<ToDoAddController>().setFormData(
+                            key: 'title',
+                            value: value,
+                          );
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: kMedium,
+                  ),
+                  CustomTextFormField(
+                    controller: _bodyController,
+                    labelText: 'Body',
+                    hintText: 'ToDo Body',
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                    maxLines: 4,
+                    minLine: 3,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'body is empty';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      context.read<ToDoAddController>().setFormData(
+                            key: 'body',
+                            value: value,
+                          );
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: kMedium,
+                  ),
+                  CustomTextFormField(
+                    controller: _noteController,
+                    labelText: 'Note',
+                    hintText: 'ToDo Note',
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.next,
+                    maxLines: 5,
+                    minLine: 3,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Note is empty';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      context.read<ToDoAddController>().setFormData(
+                            key: 'note',
+                            value: value,
+                          );
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: kMedium,
+                  ),
+                  SwitchListTile.adaptive(
+                    title: const Text('Status'),
+                    value: context.watch<ToDoAddController>().state.todoStatus,
+                    onChanged: (value) {
+                      context.read<ToDoAddController>().setToDoStatus(value);
+                    },
+                  )
+                ],
+              ),
             ),
           ),
         ),
@@ -128,12 +142,30 @@ class _ToDoAddScreenState extends State<ToDoAddScreen> {
         heroTag: 'saveToDo',
         onPressed: () {
           final isValid = _formKey.currentState?.validate();
-          if(isValid != null && isValid) {
+          if (isValid != null && isValid) {
             context.read<ToDoAddController>().addToDo();
           }
         },
         child: const Icon(Icons.save),
       ),
+    );
+  }
+
+  void _showSuccessDialog() {
+    showSuccessDialog(
+      context: context, 
+      title: 'Success', 
+      msg: 'ToDo Added Successfully', 
+      btnOkText: 'OK', 
+      onOkTap: () {
+        final navigator = Navigator.of(context, rootNavigator: true);
+        if(navigator.canPop()) {
+          // pop the dialog
+          navigator.pop();
+          // pop the route
+          context.pop();
+        }
+      },
     );
   }
 }
