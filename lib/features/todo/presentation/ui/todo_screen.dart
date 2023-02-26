6cdaf6fe8b8d207b0e62a7e809e3946fffe7ac3c
richard <richard@maqe.com>
2023-02-26 08:01:56 +0700

    list view loading status

--- lib/features/todo/presentation/ui/todo_screen.dart
+++ lib/features/todo/presentation/ui/todo_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:todo_app/common/mixin/loading_overlay_mixin.dart';
 import 'package:todo_app/common/style/dimens.dart';
 import 'package:todo_app/features/todo/domain/model/todo_item.dart';
 import 'package:todo_app/features/todo/presentation/controller/todo_controller.dart';
@@ -13,7 +14,9 @@ class ToDoScreen extends StatefulWidget {
   State<ToDoScreen> createState() => _ToDoScreenState();
 }
 
-class _ToDoScreenState extends State<ToDoScreen> {
+class _ToDoScreenState extends State<ToDoScreen> with LoadingOverlayMixin{
+  OverlayEntry? _overlayEntry;
+
   @override
   void initState() {
     super.initState();
@@ -29,55 +32,66 @@ class _ToDoScreenState extends State<ToDoScreen> {
         title: const Text('Todo'),
         centerTitle: true,
       ),
-      body: BlocSelector<ToDoController, ToDoState, List<ToDoItem>>(
-        selector: (state) {
-          return state.todos;
+      body: BlocListener<ToDoController, ToDoState>(
+        listenWhen: (previous, current) {
+          return current.isLoading != previous.isLoading;
         },
-        builder: (context, state) {
-          return ListView.builder(
-            itemCount: state.length,
-            itemBuilder: (context, index) {
-              final todo = state[index];
-              return Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(kSmall),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Text(todo.id.toString()),
-                          const Spacer(),
-                          Text(todo.createdAt),
-                        ],
-                      ),
-
-                      const SizedBox(height: kSmall,),  
-
-                      Text(todo.title),
-
-                      const SizedBox(height: kSmall,),  
-
-                      Text(todo.body),
-
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: todo.status 
-                        ? const Icon(
-                          Icons.check_circle,
-                          color: Colors.green,
+        listener: (context, state) {
+          _overlayEntry?.remove();
+          _overlayEntry = null;
+          
+          if (state.isLoading) {
+            _overlayEntry = showLoadingOverlay(context, _overlayEntry);
+          }
+        },
+        child: BlocSelector<ToDoController, ToDoState, List<ToDoItem>>(
+          selector: (state) {
+            return state.todos;
+          },
+          builder: (context, state) {
+            return ListView.builder(
+              itemCount: state.length,
+              itemBuilder: (context, index) {
+                final todo = state[index];
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(kSmall),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(todo.id.toString()),
+                            const Spacer(),
+                            Text(todo.createdAt),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: kSmall,
+                        ),
+                        Text(todo.title),
+                        const SizedBox(
+                          height: kSmall,
+                        ),
+                        Text(todo.body),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: todo.status
+                              ? const Icon(
+                                  Icons.check_circle,
+                                  color: Colors.green,
+                                )
+                              : const SizedBox.shrink(),
                         )
-                        : const SizedBox.shrink(),
-                      )                  
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              );
-
-            },
-          );
-        },
+                );
+              },
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton.small(
         heroTag: 'addToDo',
