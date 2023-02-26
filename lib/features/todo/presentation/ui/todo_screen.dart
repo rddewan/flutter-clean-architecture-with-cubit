924d3c14ae0a6d57df96182c0c6f0ddbfe9a56fa
richard <richard@maqe.com>
2023-02-26 07:51:08 +0700

    bloc selector and todo list view

--- lib/features/todo/presentation/ui/todo_screen.dart
+++ lib/features/todo/presentation/ui/todo_screen.dart
@@ -1,10 +1,26 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:todo_app/common/style/dimens.dart';
+import 'package:todo_app/features/todo/domain/model/todo_item.dart';
+import 'package:todo_app/features/todo/presentation/controller/todo_controller.dart';
+import 'package:todo_app/features/todo/presentation/state/todo_state.dart';
 
-class ToDoScreen extends StatelessWidget {
+class ToDoScreen extends StatefulWidget {
   const ToDoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ToDoScreen> createState() => _ToDoScreenState();
+}
+
+class _ToDoScreenState extends State<ToDoScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      context.read<ToDoController>().getToDos();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +29,56 @@ class ToDoScreen extends StatelessWidget {
         title: const Text('Todo'),
         centerTitle: true,
       ),
-      body: Column(),
+      body: BlocSelector<ToDoController, ToDoState, List<ToDoItem>>(
+        selector: (state) {
+          return state.todos;
+        },
+        builder: (context, state) {
+          return ListView.builder(
+            itemCount: state.length,
+            itemBuilder: (context, index) {
+              final todo = state[index];
+              return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(kSmall),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(todo.id.toString()),
+                          const Spacer(),
+                          Text(todo.createdAt),
+                        ],
+                      ),
+
+                      const SizedBox(height: kSmall,),  
+
+                      Text(todo.title),
+
+                      const SizedBox(height: kSmall,),  
+
+                      Text(todo.body),
+
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: todo.status 
+                        ? const Icon(
+                          Icons.check_circle,
+                          color: Colors.green,
+                        )
+                        : const SizedBox.shrink(),
+                      )                  
+                    ],
+                  ),
+                ),
+              );
+
+            },
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton.small(
         heroTag: 'addToDo',
         onPressed: () {
--- lib/main_widget.dart
+++ lib/main_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/core/route/go_router_provider.dart';
 import 'package:todo_app/core/service_locator.dart';
 import 'package:todo_app/core/theme/color_schemes.dart';
+import 'package:todo_app/features/todo/presentation/controller/todo_controller.dart';
 
 class MainWidget extends StatelessWidget {
   const MainWidget({super.key});
@@ -12,18 +14,23 @@ class MainWidget extends StatelessWidget {
     
     final route = getIt.get<GoRouterProvider>();
 
-    return MaterialApp.router(
-      title: 'ToDo App',
-      routerConfig: route.goRouter(),
-      theme: ThemeData(  
-        useMaterial3: true,      
-        colorScheme: lightColorScheme
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => getIt.get<ToDoController>())
+      ],
+      child: MaterialApp.router(
+        title: 'ToDo App',
+        routerConfig: route.goRouter(),
+        theme: ThemeData(  
+          useMaterial3: true,      
+          colorScheme: lightColorScheme
+        ),
+        darkTheme: ThemeData(  
+          useMaterial3: true,      
+          colorScheme: darkColorScheme
+        ),
+        
       ),
-      darkTheme: ThemeData(  
-        useMaterial3: true,      
-        colorScheme: darkColorScheme
-      ),
-      
     );
   }
 }
