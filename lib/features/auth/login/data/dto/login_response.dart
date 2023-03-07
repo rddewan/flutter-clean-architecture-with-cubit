f8e844937fb5c9abc00e8e74b3c3ad12901c70ab
richard <richard@maqe.com>
2023-03-07 07:55:47 +0700

    login dto

---
+++ lib/features/auth/login/data/dto/login_response.dart
@@ -0,0 +1,25 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'dart:convert';
+
+part 'login_response.freezed.dart';
+part 'login_response.g.dart';
+
+LoginResponse loginResponseFromJson(String str) => LoginResponse.fromJson(json.decode(str));
+
+String loginResponseToJson(LoginResponse data) => json.encode(data.toJson());
+
+@freezed
+class LoginResponse with _$LoginResponse {
+  const factory LoginResponse({
+    @JsonKey(name: 'access_token')
+    required String accessToken,
+    @JsonKey(name: 'token_id')
+    required String tokenId,
+    @JsonKey(name: 'user_id')
+    required int userId,
+    required String name,
+    required String email,
+  }) = _LoginResponse;
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) => _$LoginResponseFromJson(json);
+}
